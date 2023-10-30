8fa191c8924c8e3d8946525eca4077d9bb2253c3
Adarsh Hari Lohar <117507791+adarshhlohar@users.noreply.github.com>
2023-10-30 23:29:06 +0530

    Rotation Solution in js 30-10-23

---
+++ circularArrayRotation.js
@@ -0,0 +1,80 @@
+// John Watson knows of an operation called a right circular rotation on an array of integers. One rotation operation moves the last array element to the first position and shifts all remaining elements right one. To test Sherlock's abilities, Watson provides Sherlock with an array of integers. Sherlock is to perform the rotation operation a number of times then determine the value of the element at a given position.
+
+// For each array, perform a number of right circular rotations and return the values of the elements at the given indices.
+
+
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'circularArrayRotation' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts following parameters:
+ *  1. INTEGER_ARRAY a
+ *  2. INTEGER k
+ *  3. INTEGER_ARRAY queries
+ */
+
+function circularArrayRotation(arr, k, queries) {
+//create a copy of a blank arr that matches the length of arr
+    const rotated = new Array(arr.length)
+    for (let i = 0; i < arr.length; i++) {
+        //see the remainder index location after it is rotated rotated[(i + k) % arr.length] and assign  arr[i] to it.
+        rotated[(i + k) % arr.length] = arr[i]
+    }
+        //find the requested indexes.
+    const result = []
+    for (let j of queries) {
+        result.push(rotated[j])
+    }
+    return result
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const firstMultipleInput = readLine().replace(/\s+$/g, '').split(' ');
+
+    const n = parseInt(firstMultipleInput[0], 10);
+
+    const k = parseInt(firstMultipleInput[1], 10);
+
+    const q = parseInt(firstMultipleInput[2], 10);
+
+    const a = readLine().replace(/\s+$/g, '').split(' ').map(aTemp => parseInt(aTemp, 10));
+
+    let queries = [];
+
+    for (let i = 0; i < q; i++) {
+        const queriesItem = parseInt(readLine().trim(), 10);
+        queries.push(queriesItem);
+    }
+
+    const result = circularArrayRotation(a, k, queries);
+
+    ws.write(result.join('\n') + '\n');
+
+    ws.end();
+}
